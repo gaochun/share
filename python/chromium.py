b0d1947b76c0e5efff0d90b8f6286d46af7f519a
Yang Gu <yang.gu@intel.com>
2014-07-19 15:36:44 +0800

    chromium.py: Fix the verion of ver_no_android_gyp

--- python/chromium.py
+++ python/chromium.py
@@ -95,7 +95,7 @@ ver_gyp_defines = '37.0.0.0'
 
 # From this rev, android_gyp is no longer supported. Use gyp_chromium instead.
 rev_no_android_gyp = 262292
-ver_no_android_gyp = '37.0.1847.62'
+ver_no_android_gyp = '34.0.1847.62'
 
 # repo type specific variables
 ver = ''

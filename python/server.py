27bd09a32daa5868e698799628cab3e231753a48
Yang Gu <yang.gu@intel.com>
2014-07-21 12:25:54 +0800

    server.py: Add config for ubuntu-ygu5-02

--- python/server.py
+++ python/server.py
@@ -7,7 +7,8 @@ from util import *
 
 cb_interval = {
     'update_share': 1800,
-    'test_x64': 24 * 3600,
+    'test_x64_all': 24 * 3600,
+    'test_x64_aosp_build': 24 * 3600,
 }
 
 
@@ -22,31 +23,34 @@ def update_share():
     restore_dir()
 
 
-def test_x64():
+def test_x64_all():
     execute('python ' + dir_python + '/test-x64.py --target-arch x86_64,x86')
 
 
+def test_x64_aosp_build():
+    execute('python ' + dir_python + '/test-x64.py --target-arch x86_64,x86 --phase aosp-prebuild,aosp-build --dir-aosp aosp-stable-daily', interactive=True)
+
+
 def run():
-    while True:
-        cb = 'update_share'
-        _run_one(cb, cb_interval[cb])
+    if host_name == 'wp-01':
+        _run_one('update_share')
+        _run_one('test_x64_all')
 
-        if host_name == 'wp-01':
-            cb = 'test_x64'
-            _run_one(cb, cb_interval[cb])
+    elif host_name == 'wp-02':
+        _run_one('update_share')
 
-        elif host_name == 'wp-02':
-            pass
+    elif host_name == 'wp-03':
+        _run_one('update_share')
+        # chrome-android build
 
-        elif host_name == 'wp-03':
-            # chrome-android build
-            pass
+    elif host_name == 'ubuntu-ygu5-02':
+        _run_one('test_x64_aosp_build')
 
 
 # If callback does not start within interval, start it
-def _run_one(cb, interval):
+def _run_one(cb):
     file_cb = dir_server_log + '/' + cb
-    if not os.path.exists(file_cb) or not has_recent_change(file_cb, interval=interval):
+    if not os.path.exists(file_cb) or not has_recent_change(file_cb, interval=cb_interval[cb]):
         execute('touch ' + file_cb)
         globals()[cb]()
 

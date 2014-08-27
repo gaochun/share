b884a1e4048ae2f65179f75091493f05726c325a
Yang Gu <yang.gu@intel.com>
2014-08-27 11:25:02 +0800

    webmark: Add guimark3compute

---
+++ python/webmark/benchmark/guimark3compute.py
@@ -0,0 +1,35 @@
+﻿from benchmark import *
+
+
+class guimark3compute(Benchmark):
+    CONFIG = {
+        'name': 'GUIMark3 Compute',
+        'metric': 'FPS',
+        'path': {
+            'external': 'http://www.craftymind.com/factory/guimark3/compute/GM3_JS_Compute.html',
+            'internal': 'webbench/GUIMark3/compute/GM3_JS_Compute.html'
+        },
+        'times_run': 3,
+    }
+
+    def cond0(self, driver):
+        self.e = driver.find_element_by_id('testaction')
+        if self.e:
+            return True
+        else:
+            return False
+
+    def act0(self, driver):
+        self.e.click()
+
+    def cond1(self, driver):
+        self.e = driver.find_element_by_id('testlabel')
+        if self.e.text.find('Test Results:') != -1:
+            return True
+        else:
+            return False
+
+    def act1(self, driver):
+        pattern = re.compile('(\d+\.?\d*) fps')
+        match = pattern.search(self.e.text)
+        self.result.append(match.group(1))

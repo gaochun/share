67342a6575ee9284c13f091e4be3367aa3783253
Yang Gu <yang.gu@intel.com>
2014-09-02 03:15:55 +0800

    upgrade.py: Update using local http server

--- python/upgrade.py
+++ python/upgrade.py
@@ -1,53 +1,39 @@
 from util import *
 
 
-def handle_option():
+def parse_arg():
     global args
     parser = argparse.ArgumentParser(description='Script to upgrade system',
                                      formatter_class=argparse.RawTextHelpFormatter,
                                      epilog='''
 Examples:
   python %(prog)s
-  python %(prog)s -t chrome
+  python %(prog)s -t basic
   python %(prog)s -t system
 ''')
-    parser.add_argument('-t', '--type', dest='type', help='type to upgrade', default='basic', choices=['basic', 'chrome', 'system'])
+    parser.add_argument('-t', '--type', dest='type', help='type to upgrade', choices=['basic', 'system'])
     args = parser.parse_args()
 
     if len(sys.argv) <= 1:
         parser.print_help()
 
 
+def setup():
+    set_proxy()
+    execute('echo \'Acquire::http::proxy "http://127.0.0.1:8118";\' >apt.conf', show_cmd=False)
+    execute('sudo mv apt.conf /etc/apt/', show_cmd=False)
+    execute('echo \'deb https://dl.google.com/linux/chrome/deb/ stable main\' >google.list', show_cmd=False)
+    execute('sudo mv google.list /etc/apt/sources.list.d/', show_cmd=False)
+
+
 def upgrade():
     if args.type == 'basic':
-        setup_http()
         execute('sudo apt-get update && sudo apt-get -y dist-upgrade', interactive=True)
-    elif args.type == 'chrome':
-        setup_socks()
-        execute('sudo tsocks apt-get update && sudo tsocks apt-get -y dist-upgrade', interactive=True)
-        setup_http()
     elif args.type == 'system':
-        setup_http()
         execute('sudo update-manager -d', interactive=True)
 
 
-def remove(path):
-    if os.path.exists(path):
-        execute('sudo rm -f ' + path)
-
-
-def setup_http():
-    execute('echo \'Acquire::http::proxy "http://proxy-shz.intel.com:911";\' >apt.conf')
-    execute('sudo mv apt.conf /etc/apt/')
-    remove('/etc/apt/sources.list.d/google.list')
-
-
-def setup_socks():
-    remove('/etc/apt/apt.conf')
-    execute('echo \'deb https://dl.google.com/linux/chrome/deb/ stable main\' >google.list')
-    execute('sudo mv google.list /etc/apt/sources.list.d/')
-
-
 if __name__ == "__main__":
-    handle_option()
+    parse_arg()
+    setup()
     upgrade()

7cfcf45aaf50df364c049ae145ee318900efcc40
YuZhiqiangX <zhiqiangx.yu@intel.com>
2014-09-01 15:48:30 +0800

    webmark: Add benchmark galactic and guimark3bitmap

---
+++ python/webmark/benchmark/galactic.py
@@ -0,0 +1,45 @@
+from benchmark import *
+
+
+class galactic(Benchmark):
+    CONFIG = {
+        'name': 'Galactic',
+        'metric': 'FPS',
+        'path': {
+            'desktop': {
+                'external': 'http://ie.microsoft.com/testdrive/Performance/Galactic/Default.html',
+                'internal': 'webbench/microsoft/testdrive/Performance/Galactic/Default.html'
+            },
+            'mobile': {
+                'external': 'http://ie.microsoft.com/testdrive/Performance/Galactic/Default.html',
+                'internal': 'webbench/microsoft/testdrive/mobile/Performance/Galactic/'
+            }
+        },
+        'version': 'mobile',
+    }
+
+    def __init__(self, driver, case):
+        super(galactic, self).__init__(driver, case)
+
+    def cond0(self, driver):
+        if self.version == 'mobile':
+            self.e = driver.find_element_by_id('FPS_text')
+            respite = 0
+        elif self.version == 'desktop':
+            self.e = driver.find_element_by_id('miniview')
+            respite = 3
+        else:
+            error('version: %s is not supported' % self.version)
+
+        if self.e:
+            time.sleep(respite)
+            return True
+        else:
+            return False
+
+    def act0(self, driver):
+        self.result = self.get_result_periodic(driver)
+
+    def get_result_one(self, driver):
+        return str(driver.execute_script('return gFpsData.AvgFps'))
+

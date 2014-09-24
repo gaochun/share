a58ef56cf691f1216896ed2d31aad54054111c40
Yang Gu <yang.gu@intel.com>
2014-09-25 00:29:44 +0800

    album.py: Align with folder structure change

--- python/album.py
+++ python/album.py
@@ -1,88 +1,67 @@
 #! N:\Python27\python.exe
 # -*- coding: utf-8 -*-
 
-import re
-import time
-import os
-import sys
-import shutil
+from util import *
 
-script_dir = sys.path[0]
-check_dirs = [u'24月']
-
-origin_dir = u'e:/storage/origin/'
-storage_dir = 'e:/storage/'
-googleplus_storage_dir = storage_dir + 'GooglePlus/'
-skydrive_storage_dir = storage_dir + u'SkyDrive/顾诗云/'
-
+dirs_check = [u'025']
+dir_root = u'e:/storage/顾诗云'
 ffmpeg = 'D:/software/active/ffmpeg/bin/ffmpeg.exe'
 
-def info(msg):
-    print '[INFO] ' + msg + '.'
 
-def has_file(dir, name):
-    files = os.listdir(dir)
-    for file in files:
-        if file == name:
-            return True
+def setup():
+    backup_dir(dir_root)
 
-    return False
-
-def lock(name):
-    f = open(name, 'w')
-    f.close()
-
-def unlock(name):
-    os.remove(name)
-
-def src_to_dest(src_dir, dest_dir):
-    for check_dir in check_dirs:
-        src_check_dir = src_dir + check_dir + '/'
-        if not os.path.exists(src_check_dir):
+def process():
+    for dir_check in dirs_check:
+        if not os.path.exists(dir_check):
             continue
 
-        dest_check_dir = dest_dir + check_dir + '/'
-        if not os.path.exists(dest_check_dir):
-            os.mkdir(dest_check_dir)
-        src_files = os.listdir(src_check_dir)
-        for src_file in src_files:
-            src_file_suffix = src_file[-4:].upper()
-            src_file_path = src_check_dir + src_file
-            if src_file_suffix == '.MOV':
-                dest_file = src_file.replace(src_file[-4:], '.MP4')
+        dir_photo_good = dir_check + u'月照片好'
+        ensure_dir(dir_photo_good)
+        dir_photo_all = dir_check + u'月照片全'
+        ensure_dir(dir_photo_all)
+        dir_video_1080 = dir_check + u'月视频1080'
+        ensure_dir(dir_video_1080)
+        dir_video_720 = dir_check + u'月视频720'
+        ensure_dir(dir_video_720)
+
+        files_src = os.listdir(dir_check)
+        for file_src in files_src:
+            file_src_suffix = file_src[-4:].upper()
+            file_src_path = dir_check + '/' + file_src
+            if file_src_suffix == '.MOV':
+                file_dest = file_src.replace(file_src[-4:], '.MP4')
             else:
-                dest_file = src_file
-            dest_file_path= dest_check_dir + dest_file
+                file_dest = file_src
 
-            if os.path.exists(dest_file_path):
-                continue
-
-            if src_file_suffix == '.JPG':
-                info('Copy file ' + src_file_path + ' to ' + dest_file_path)
-                shutil.copyfile(src_file_path, dest_file_path)
-            elif src_file_suffix == '.MP4':
-                if src_dir == origin_dir:
-                    info('Copy file ' + src_file_path + ' to ' + dest_file_path)
-                    shutil.copyfile(src_file_path, dest_file_path)
+            if file_src_suffix == '.JPG':
+                file_dest_path = dir_photo_all + '/' + file_dest
+                if not os.path.exists(file_dest_path):
+                    execute('mv %s %s' % (file_src_path, file_dest_path))
                 else:
-                    command = (ffmpeg + ' -i ' + src_file_path + ' -qscale 0 -s hd720 -f mp4 ' + dest_file + ' 2>>NUL').encode(sys.getfilesystemencoding())
-                    info('Convert file ' + src_file_path + ' (1080P) to ' + dest_file_path + ' (720P)')
-                    os.system(command)
-                    os.rename(dest_file, dest_file_path)
-            elif src_file_suffix == '.MOV':
-                command = (ffmpeg + ' -i ' + src_file_path + ' -qscale 0 -s hd1080 -f mp4 ' + dest_file + ' 2>>NUL').encode(sys.getfilesystemencoding())
-                info('Convert file ' + src_file_path + ' to ' + dest_file_path)
-                os.system(command)
-                os.rename(dest_file, dest_file_path)
+                    warning(file_dest_path + ' already exists')
+            elif file_src_suffix == '.MP4' or file_src_suffix == '.MOV':
+                # handle 1080p
+                file_dest_path = dir_video_1080 + '/' + file_dest
+                if not os.path.exists(file_dest_path):
+                    if file_src_suffix == '.MP4':
+                        execute('mv %s %s' % (file_src_path, file_dest_path))
+                    elif file_src_suffix == '.MOV':
+                        info('Convert file ' + file_src_path + ' to ' + file_dest_path)
+                        cmd = (ffmpeg + ' -i ' + file_src_path + ' -qscale 0 -s hd1080 -f mp4 ' + file_dest + ' 2>>NUL').encode(sys.getfilesystemencoding())
+                        execute(cmd, show_cmd=False)
+                        os.rename(file_dest, file_dest_path)
+
+                # handle 720p
+                file_src_path = file_dest_path
+                file_dest_path = dir_video_720 + '/' + file_dest
+                if not os.path.exists(file_dest_path):
+                    cmd = (ffmpeg + ' -i ' + file_src_path + ' -qscale 0 -s hd720 -f mp4 ' + file_dest + ' 2>>NUL').encode(sys.getfilesystemencoding())
+                    info('Convert file ' + file_src_path + ' (1080P) to ' + file_dest_path + ' (720P)')
+                    execute(cmd, show_cmd=False)
+                    os.rename(file_dest, file_dest_path)
+
 
 if __name__ == '__main__':
-    os.chdir(script_dir)
-    lock_file = 'lock'
-    if has_file(script_dir, lock_file):
-        quit()
-
-    #lock(lock_file)
-    src_to_dest(origin_dir, googleplus_storage_dir)
-    src_to_dest(googleplus_storage_dir, skydrive_storage_dir)
-    #unlock(lock_file)
-
+    setup()
+    process()

583a244d3054319e6a28b0fe61a4375999b9a40f
Yang Gu <yang.gu@intel.com>
2014-11-04 11:21:19 +0800

    elf.py: First cry for elf file analysis

---
+++ python/elf.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+from util import *
+import operator
+
+elf = ''
+
+
+def parse_arg():
+    global args, args_dict
+    parser = argparse.ArgumentParser(description='Script to handle elf',
+                                     formatter_class=argparse.RawTextHelpFormatter,
+                                     epilog='''
+examples:
+  python %(prog)s --revert -s --patch -b
+''')
+    parser.add_argument('--elf', dest='elf', help='elf file')
+    parser.add_argument('--size', dest='size', help='size', action='store_true')
+
+    args = parser.parse_args()
+    args_dict = vars(args)
+
+    if len(sys.argv) <= 1:
+        parser.print_help()
+        quit()
+
+
+def setup():
+    global elf
+
+    elf = args.elf
+
+
+def size():
+    if not args.size:
+        return
+
+    name_size = {}
+    lines = _get_lines(cmd='readelf -S ' + elf)
+    for line in lines:
+        match = re.search('\[\s*(\d+)\]', line)
+        if match:
+            if int(match.group(1)) == 0:
+                continue
+            fields = line.split()
+            if int(match.group(1)) < 10:
+                fields[1] = fields[0] + fields[1]
+                del fields[0]
+
+            name_size[fields[1]] = int(fields[5], 16)
+
+    sorted_size = sorted(name_size.items(), key=lambda x: x[1], reverse=True)
+    for item in sorted_size:
+        print item[0] + ' ' + str(item[1])
+
+
+def _get_lines(cmd):
+    result = execute(cmd, return_output=True)
+    return result[1].split('\n')
+
+
+if __name__ == '__main__':
+    parse_arg()
+    setup()
+    size()

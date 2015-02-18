3be9c6f025303f828c44a2aa7dcf02e11a2f87c6
Yang Gu <yang.gu@intel.com>
2015-02-18 18:46:33 +0800

    album.py: Sort the files with pic first

--- python/album.py
+++ python/album.py
@@ -4,29 +4,30 @@
 from util import *
 import shutil
 
-dirs_check = [u'026']
-dir_root = u'e:/storage/顾诗云'
+dirs_check = [u'030']
+dir_root = u'e:/storage'
 ffmpeg = 'D:/software/active/ffmpeg/bin/ffmpeg.exe'
 
 
 def setup():
     backup_dir(dir_root)
 
+
 def process():
     for dir_check in dirs_check:
         if not os.path.exists(dir_check):
             continue
 
-        dir_photo_good = dir_check + u'月照片好'
+        dir_photo_good = u'顾云/' + dir_check + u'月照片好'
         ensure_dir(dir_photo_good)
-        dir_photo_all = dir_check + u'月照片全'
+        dir_photo_all = u'顾云/' + dir_check + u'月照片全'
         ensure_dir(dir_photo_all)
-        dir_video_1080 = dir_check + u'月视频1080'
-        ensure_dir(dir_video_1080)
-        dir_video_720 = dir_check + u'月视频720'
+        dir_video_720 = u'顾云/' + dir_check + u'月视频720'
         ensure_dir(dir_video_720)
+        dir_video_1080 = u'顾云旧/' + dir_check + u'月视频1080'
+        ensure_dir(dir_video_1080)
 
-        files_src = os.listdir(dir_check)
+        files_src = sorted(os.listdir(dir_check), reverse=True)
         for file_src in files_src:
             file_src_suffix = file_src[-4:].upper()
             file_src_path = dir_check + '/' + file_src
